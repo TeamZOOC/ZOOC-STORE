1717ad35af2231ed9658e38584b16e2017f3f235
Dev-jwJeong <rgn95652@gmail.com>
2023-10-31 20:38:15 +0900

    :lipstick: 헤더 컴포넌트 이미지(#6)

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,4 +1,7 @@
 import IcBack from './ic_back.svg';
 import IcZooc from './ic_zooc.svg';
+import IcOrderList from './ic_orderlist.svg';
+import IcCart from './ic_cart.svg';
+import IcExit from './ic_exit.svg';
 
-export { IcBack, IcZooc };
+export { IcBack, IcZooc, IcOrderList, IcCart, IcExit };
