02c9f848b705e99f65310fe229f8be1a7dc0f6c3
Dev-jwJeong <rgn95652@gmail.com>
2023-11-01 17:42:32 +0900

    :lipstick: 홈페이지 컴포넌트 쌓기(#10)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
-'use client';
-
-import { styled } from 'styled-components';
-
-import { IcBack, IcZooc } from '../../public/icons';
+import { MainLayout } from '@/components/layout';
+import { ARTICLE_LIST } from '@/mocks/articleData';
+import { HomeArticle, HomeCategory, HomeProduct } from './(home)';
 
 const page = () => (
-  <div>
-    <IcBack />
-    <IcZooc />
-    <StGmarket>지마켓산스 폰트 GmarketSans price_big ZOOC</StGmarket>
-    <StPretendard>Subhead1 프리텐다드 폰트 Pretendard</StPretendard>
-  </div>
+  <MainLayout>
+    <HomeCategory />
+    <HomeArticle article={ARTICLE_LIST[0]} />
+    <HomeProduct />
+    <HomeArticle article={ARTICLE_LIST[1]} />
+    <HomeArticle article={ARTICLE_LIST[2]} />
+  </MainLayout>
 );
 
 export default page;
-
-const StGmarket = styled.p`
-  color: ${({ theme }) => theme.colors.zw_point};
-  ${({ theme }) => theme.fonts.zw_price_big};
-`;
-
-const StPretendard = styled.p`
-  color: ${({ theme }) => theme.colors.zw_darkgray};
-  ${({ theme }) => theme.fonts.zw_Subhead1};
-`;
