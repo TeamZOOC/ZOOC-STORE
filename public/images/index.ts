a9304d5d2e7f52202e283686b6511337ef740bdf
Dev-jwJeong <rgn95652@gmail.com>
2023-11-02 21:35:55 +0900

    :lipstick: 홈페이지 테스트 이미지(#10)

---
+++ public/images/index.ts
@@ -0,0 +1,5 @@
+import ImageArticle from './image_article.png';
+import ImageCarusel from './image_carusel.png';
+import ImageProduct from './image_product.png';
+
+export { ImageArticle, ImageCarusel, ImageProduct };
