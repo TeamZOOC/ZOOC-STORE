f80970134e99b808b4c1a26ad0d71a50060979d3
Dev-jwJeong <rgn95652@gmail.com>
2023-11-02 21:42:23 +0900

    :lipstick: 홈 페이지 레이아웃 / 헤더 적용(#10)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import { MainLayout } from '@/components/layout';
 import { ARTICLE_LIST } from '@/mocks/articleData';
+import { Header } from '@/components/header';
 import { HomeArticle, HomeCategory, HomeProduct } from './(home)';
 import HomeCarusel from './(home)/carusel/HomeCarusel';
 
 const page = () => (
-  <MainLayout>
-    <HomeCarusel />
-    <HomeCategory />
-    <HomeArticle article={ARTICLE_LIST[0]} />
-    <HomeProduct />
-    <HomeArticle article={ARTICLE_LIST[1]} />
-    <HomeArticle article={ARTICLE_LIST[2]} />
-  </MainLayout>
+  <>
+    <Header sideMenu />;
+    <MainLayout>
+      <HomeCarusel />
+      <HomeCategory />
+      <HomeArticle article={ARTICLE_LIST[0]} />
+      <HomeProduct />
+      <HomeArticle article={ARTICLE_LIST[1]} />
+      <HomeArticle article={ARTICLE_LIST[2]} />
+    </MainLayout>
+  </>
 );
 
 export default page;
