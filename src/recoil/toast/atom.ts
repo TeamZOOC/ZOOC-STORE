c6d90ad2a55017e4b2615c0cb326af32dad0712d
parkhyeonji <iamphj3@naver.com>
2023-11-03 16:58:31 +0900

    :label: toast atom, selector 선언(#12)

---
+++ src/recoil/toast/atom.ts
@@ -0,0 +1,22 @@
+import { atom, atomFamily } from 'recoil';
+
+const TOAST_TYPE = ['delivery', 'agreement'] as const;
+
+export type ToastType = (typeof TOAST_TYPE)[number];
+export type Toast = {
+  id: ToastType;
+  message: string;
+};
+
+export const toastState = atomFamily<Toast, ToastType>({
+  key: 'toastState',
+  default: (id) => ({
+    id,
+    message: '',
+  }),
+});
+
+export const toastListState = atom<Toast[]>({
+  key: 'toastStateState',
+  default: [],
+});
