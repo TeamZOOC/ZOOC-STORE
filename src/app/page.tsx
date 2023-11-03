a9bd9fcef05d6d8e667a146ef8b9437808800bd0
parkhyeonji <iamphj3@naver.com>
2023-11-03 16:35:50 +0900

    :twisted_rightwards_arrows: develop merge(#4)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-import TestClientComponent from './TestClientComponent';
+import { Header } from '@/components/header';
+import { MainLayout } from '@/components/layout';
+import { ARTICLE_LIST } from '@/mocks/articleData';
 
-const page = () => <TestClientComponent />;
+import { HomeArticle, HomeCategory, HomeProduct } from './(home)';
+import HomeCarusel from './(home)/carusel/HomeCarusel';
+
+const page = () => (
+  <>
+    <Header sideMenu />;
+    <MainLayout>
+      <HomeCarusel />
+      <HomeCategory />
+      <HomeArticle article={ARTICLE_LIST[0]} />
+      <HomeProduct />
+      <HomeArticle article={ARTICLE_LIST[1]} />
+      <HomeArticle article={ARTICLE_LIST[2]} />
+    </MainLayout>
+  </>
+);
 
 export default page;
