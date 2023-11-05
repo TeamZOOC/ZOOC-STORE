3c4f632e6f5d64a8e6d07c48b60da93b176994f0
parkhyeonji <iamphj3@naver.com>
2023-11-06 02:52:05 +0900

    :lipstick: 주문내역 마지막 항목에는 구분선 생기지 않도록 변경(#3)

--- src/app/order/list/OrderList.tsx
+++ src/app/order/list/OrderList.tsx
@@ -24,7 +24,7 @@ const OrderList = () => {
             order={orderInfo}
             onDetailClick={handleOrderDetail}
           />
-          <StHr />
+          {orderList.indexOf(orderInfo) !== orderList.length - 1 && <StHr />}
         </>
       ))}
     </StOrderListWrapper>
