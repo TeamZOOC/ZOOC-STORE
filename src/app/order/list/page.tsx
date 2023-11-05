ff7fc4e187fbd4ab93ebcc8b7aca426db09ac672
parkhyeonji <iamphj3@naver.com>
2023-11-06 02:35:57 +0900

    :lipstick: 주문내역 헤더, 메인레이아웃 적용(#3)

--- src/app/order/list/page.tsx
+++ src/app/order/list/page.tsx
@@ -1,5 +1,15 @@
+import { Header } from '@/components/header';
+import { MainLayout } from '@/components/layout';
+
 import OrderList from './OrderList';
 
-const page = () => <OrderList />;
+const page = () => (
+  <>
+    <Header headerTitle="주문내역" />
+    <MainLayout>
+      <OrderList />
+    </MainLayout>
+  </>
+);
 
 export default page;
