1838f3152882a51c95f35d2c64a0dce8b51f8ba1
parkhyeonji <iamphj3@naver.com>
2023-11-07 18:23:16 +0900

    :label: addressState recoil atom 선언(#22)

---
+++ src/recoil/order/atom.ts
@@ -0,0 +1,17 @@
+import { atom } from 'recoil';
+
+// 임시 interface 선언
+export interface AddressInfo {
+  address: string;
+  addressDetail?: string;
+  postcode: string;
+}
+
+export const addressState = atom<AddressInfo>({
+  key: 'addressState',
+  default: {
+    address: '',
+    addressDetail: undefined,
+    postcode: '',
+  },
+});
