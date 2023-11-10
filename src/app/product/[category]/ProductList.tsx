ea0adc3fab7b0bfa8fe4e2a3e5e4ccc85890f10b
Dev-jwJeong <rgn95652@gmail.com>
2023-11-10 16:16:59 +0900

    :sparkles: 상품 전체 목록에서 상세보기 라우팅 기능 구현

--- src/app/product/[category]/ProductList.tsx
+++ src/app/product/[category]/ProductList.tsx
@@ -2,24 +2,31 @@
 
 import { ProductItem } from '@/components/product';
 import { PRODUCT_LIST } from '@/mocks/productListData';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { styled } from 'styled-components';
 
-const ProductList = () => (
-  <StProductList>
-    {PRODUCT_LIST.map((product) => (
-      <ProductItem key={product.id} product={product} />
-    ))}
-  </StProductList>
-);
-
+const ProductList = () => {
+  const pathname = usePathname();
+  return (
+    <StProductList>
+      {PRODUCT_LIST.map((product) => (
+        <Link
+          href={`/product/${pathname.slice(9)}/${product.id}`}
+          key={product.id}
+        >
+          <ProductItem product={product} />
+        </Link>
+      ))}
+    </StProductList>
+  );
+};
 export default ProductList;
-
 const StProductList = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   row-gap: 2.8rem;
   column-gap: 1.1rem;
-
   width: 100%;
   margin-top: 2rem;
 `;
