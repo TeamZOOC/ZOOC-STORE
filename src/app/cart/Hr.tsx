7748c720b5ae6c70b44b4a12d234a529b8607792
Dev-jwJeong <rgn95652@gmail.com>
2023-11-14 18:31:45 +0900

    :lipstick: 구분선 hr 태그로 변경(#34)

--- src/app/cart/Hr.tsx
+++ src/app/cart/Hr.tsx
@@ -6,9 +6,10 @@ const Hr = () => <StHr />;
 
 export default Hr;
 
-const StHr = styled.div`
+const StHr = styled.hr`
   height: 0.1rem;
   margin: 4rem 0 2.4rem 2.8rem;
 
-  background: ${({ theme }) => theme.colors.zw_brightgray};
+  border: none;
+  background-color: ${({ theme }) => theme.colors.zw_brightgray};
 `;
