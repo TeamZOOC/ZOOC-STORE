86bc2171aaa223f14a0a95b23b5a418c607bb60b
parkhyeonji <iamphj3@naver.com>
2023-11-14 18:49:49 +0900

    :truck: 동의 항목 상수 폴더로 이동(#22)

--- src/app/order/(order)/agreement/Agreement.tsx
+++ src/app/order/(order)/agreement/Agreement.tsx
@@ -2,12 +2,7 @@ import { useFormContext } from 'react-hook-form';
 import { styled } from 'styled-components';
 
 import { CheckBox } from '@/components/form';
-
-const ORDER_AGREEMENT = [
-  { id: 'checkOrder', label: '주문 내역을 모두 확인했으며, 동의합니다.' },
-  { id: 'privacyPolicy', label: '개인정보 이용약관 동의 (필수)' },
-  { id: 'thirdParty', label: '제3자 정보제공 동의 (필수)' },
-];
+import { ORDER_AGREEMENT } from '@/constants/order';
 
 interface AgreementFormData {
   agreement: {
