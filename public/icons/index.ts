b64a016947d27e2695d2109f6468a6896faba02e
parkhyeonji <iamphj3@naver.com>
2023-11-14 22:01:31 +0900

    :bento: 마이페이지 아이콘 추가(#40)

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,23 +1,26 @@
+import IcApple from './ic_apple.svg';
 import IcArrowRight from './ic_arrow_right.svg';
 import IcBack from './ic_back.svg';
 import IcCart from './ic_cart.svg';
+import IcDropDown from './ic_dropdown.svg';
 import IcExit from './ic_exit.svg';
 import IcFirstStep from './ic_first_step.svg';
+import IcInquiry from './ic_inquiry.svg';
+import IcKakao from './ic_kakao.svg';
 import IcKakaoBank from './ic_kakaobank.svg';
 import IcKakaoPay from './ic_kakaopay.svg';
 import IcKey from './ic_key.svg';
+import IcLogout from './ic_logout.svg';
+import IcMinus from './ic_minus.svg';
+import IcMyPage from './ic_mypage.svg';
+import IcNotice from './ic_notice.svg';
 import IcOrderList from './ic_orderlist.svg';
+import IcPlus from './ic_plus.svg';
+import IcProductDelete from './ic_product_delete.svg';
 import IcSecondStep from './ic_second_step.svg';
 import IcThirdStep from './ic_third_step.svg';
 import IcToss from './ic_toss.svg';
 import IcZooc from './ic_zooc.svg';
-import IcMyPage from './ic_mypage.svg';
-import IcDropDown from './ic_dropdown.svg';
-import IcProductDelete from './ic_product_delete.svg';
-import IcMinus from './ic_minus.svg';
-import IcPlus from './ic_plus.svg';
-import IcApple from './ic_apple.svg';
-import IcKakao from './ic_kakao.svg';
 
 export {
   IcBack,
@@ -40,4 +43,7 @@ export {
   IcPlus,
   IcApple,
   IcKakao,
+  IcNotice,
+  IcLogout,
+  IcInquiry,
 };
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,3 +1,11 @@
-const page = () => <div>마이페이지</div>;
+import { Header } from '@/components/header';
+import { MainLayout } from '@/components/layout';
+
+const page = () => (
+  <>
+    <Header headerTitle="MY" />
+    <MainLayout>ddd</MainLayout>
+  </>
+);
 
 export default page;
