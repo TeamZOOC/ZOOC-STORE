35a378eb7104f777f505f39a9f81e0ce7cb7aa17
Dev-jwJeong <rgn95652@gmail.com>
2023-11-15 22:46:29 +0900

    :sparkles: 로그인 커스텀 훅에 카카오 accessToken props 전달(#38)

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,9 +1,14 @@
 /* eslint-disable consistent-return */
 import client from './axios';
 
-export const kakaoSignIn = async () => {
+export const kakaoSignIn = async (accessToken: string) => {
   try {
-    const response = await client.get('/user/kakao/signin');
+    const response = await client.get('/user/kakao/signin', {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
     return response.data;
   } catch (e) {
     console.error(e);
--- src/app/auth/hooks/useLogin.tsx
+++ src/app/auth/hooks/useLogin.tsx
@@ -5,7 +5,7 @@ export async function useLogin() {
   const { data: session } = useSession();
 
   if (session) {
-    const response = await kakaoSignIn();
+    const response = await kakaoSignIn(session.accessToken);
     console.log(response);
   }
 }
