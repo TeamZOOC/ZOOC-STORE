8e4eef931eab7e2dfe871802572c7454c1bad106
Dev-jwJeong <rgn95652@gmail.com>
2023-11-16 14:19:14 +0900

    :sparkles: 약관동의 체크박스 로직 적용(#42)

--- src/app/auth/signup/AgreementCheckContent.tsx
+++ src/app/auth/signup/AgreementCheckContent.tsx
@@ -2,29 +2,55 @@
 
 import { AGREEMENT } from '@/constants/agreement';
 import Link from 'next/link';
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
+import { BottomButton } from '@/components/button';
+import useCheckAgreement from '../hooks/useCheckAgreement';
+import { IcCheck } from '../../../../public/icons';
 
-const AgreementCheckContent = () => (
-  <StAgreementCheckContent>
-    <StAgreementCheckAll>
-      전체 동의
-      <StAgeementCheckBox type="button" />
-    </StAgreementCheckAll>
-    {AGREEMENT.map(({ id, agreementLink, agreementTitle }) => (
-      <StAgreementCheckItem key={id}>
-        <StAgreementCheckItemLeft>
-          <StAgeementCheckBox type="button" />
-          {agreementTitle}
-        </StAgreementCheckItemLeft>
-        <StAgreementLinkButton>
-          <Link target="_blank" href={agreementLink}>
-            보기
-          </Link>
-        </StAgreementLinkButton>
-      </StAgreementCheckItem>
-    ))}
-  </StAgreementCheckContent>
-);
+const AgreementCheckContent = () => {
+  const {
+    agreement,
+    isAllAgreement,
+    handleCheckAgreement,
+    handleCheckAllAgreement,
+  } = useCheckAgreement();
+
+  return (
+    <StAgreementCheckContent>
+      <StAgreementCheckAll $checkAll={isAllAgreement}>
+        전체 동의
+        <StAgeementCheckBox
+          type="button"
+          $check={isAllAgreement}
+          onClick={handleCheckAllAgreement}
+        >
+          {isAllAgreement && <IcCheck />}
+        </StAgeementCheckBox>
+      </StAgreementCheckAll>
+      {AGREEMENT.map(({ id, agreementLink, agreementTitle }) => (
+        <StAgreementCheckItem key={id} onClick={() => handleCheckAgreement(id)}>
+          <StAgreementCheckItemLeft>
+            <StAgeementCheckBox type="button" $check={agreement[id - 1]}>
+              {agreement[id - 1] && <IcCheck />}
+            </StAgeementCheckBox>
+            {agreementTitle}
+          </StAgreementCheckItemLeft>
+          <StAgreementLinkButton>
+            <Link target="_blank" href={agreementLink}>
+              보기
+            </Link>
+          </StAgreementLinkButton>
+        </StAgreementCheckItem>
+      ))}
+      <BottomButton
+        btnType="button"
+        btnName="회원가입"
+        disabled
+        activeFunc={() => {}}
+      />
+    </StAgreementCheckContent>
+  );
+};
 
 export default AgreementCheckContent;
 
@@ -34,7 +60,7 @@ const StAgreementCheckContent = styled.div`
   gap: 3.2rem;
 `;
 
-const StAgeementCheckBox = styled.button`
+const StAgeementCheckBox = styled.button<{ $check?: boolean }>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -42,21 +68,43 @@ const StAgeementCheckBox = styled.button`
   width: 1.8rem;
   height: 1.8rem;
 
-  border: 0.13rem solid ${({ theme }) => theme.colors.zw_lightgray};
   border-radius: 0.2rem;
+
+  ${({ $check }) =>
+    $check
+      ? css`
+          border: 0.13rem solid ${({ theme }) => theme.colors.zw_point};
+          background-color: ${({ theme }) => theme.colors.zw_point};
+        `
+      : css`
+          border: 0.13rem solid ${({ theme }) => theme.colors.zw_lightgray};
+          background-color: transparent;
+        `}
+
+  transition : all 0.1s;
 `;
-const StAgreementCheckAll = styled.div`
+const StAgreementCheckAll = styled.div<{ $checkAll: boolean }>`
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   padding: 2.1rem 2rem;
 
-  border: 0.13rem solid ${({ theme }) => theme.colors.zw_lightgray};
   border-radius: 0.2rem;
-
-  color: ${({ theme }) => theme.colors.zw_lightgray};
   ${({ theme }) => theme.fonts.zw_Subhead3}
+
+  ${({ $checkAll }) =>
+    $checkAll
+      ? css`
+          border: 0.13rem solid ${({ theme }) => theme.colors.zw_point};
+          color: ${({ theme }) => theme.colors.zw_point};
+        `
+      : css`
+          border: 0.13rem solid ${({ theme }) => theme.colors.zw_lightgray};
+          color: ${({ theme }) => theme.colors.zw_lightgray};
+        `}
+
+  transition : all 0.1s;
 `;
 
 const StAgreementCheckItem = styled.div`
