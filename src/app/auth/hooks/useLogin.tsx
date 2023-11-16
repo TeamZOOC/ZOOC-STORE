ccf50d68ff44952e14a3c5f342e6e4c86a524756
Dev-jwJeong <rgn95652@gmail.com>
2023-11-16 17:19:17 +0900

    :sparkles: 기존 유저 식별 값에 따른 라우팅 분리(#38)

--- src/app/auth/hooks/useLogin.tsx
+++ src/app/auth/hooks/useLogin.tsx
@@ -1,11 +1,21 @@
 import { kakaoSignIn } from '@/apis/auth';
+import { setCookie } from 'cookies-next';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export async function useLogin() {
+  const router = useRouter();
   const { data: session } = useSession();
 
-  if (session) {
+  if (session?.accessToken) {
     const response = await kakaoSignIn(session.accessToken);
-    console.log(response);
+    if (response) {
+      setCookie('accessToken', response.data.accessToken);
+      if (response.data.isExistedUser) {
+        router.push('/');
+      } else {
+        router.push('/auth/signup');
+      }
+    }
   }
 }
