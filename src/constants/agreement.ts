2123e8c753e6a586e6b30aef7f4419ea9d47f7eb
Dev-jwJeong <rgn95652@gmail.com>
2023-11-16 14:18:23 +0900

    :bug: 약관동의 상수 배열 id 수정(#42)

--- src/constants/agreement.ts
+++ src/constants/agreement.ts
@@ -12,13 +12,13 @@ export const AGREEMENT = [
       'https://melt-split.notion.site/65a11bf765b748678c8e6345cbfc6140?pvs=4',
   },
   {
-    id: 1,
+    id: 3,
     agreementTitle: '마케팅 정보 수신 동의 (필수)',
     agreementLink:
       'https://melt-split.notion.site/3-f551ffc7774e4560a3a3cc17dd3c39ef',
   },
   {
-    id: 1,
+    id: 4,
     agreementTitle: '만 14세 이상 확인 (필수)',
     agreementLink:
       'https://melt-split.notion.site/8c417ba2156d4720b77f480d80ace49d',
