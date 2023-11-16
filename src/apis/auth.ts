3bb9f4b7d4fc7b14338488b0a2e46e4608a5f9e5
Dev-jwJeong <rgn95652@gmail.com>
2023-11-16 17:18:52 +0900

    :sparkles: 카카오 토큰 활용해서 자체 서버 로그인 기능 구현(#38)

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,14 +1,22 @@
 /* eslint-disable consistent-return */
-import client from './axios';
+import { KakaoSignInResponse } from '@/types/auth';
+import { AxiosResponse } from 'axios';
+import { generalAxios } from './axios';
 
-export const kakaoSignIn = async (accessToken: string) => {
+export const kakaoSignIn = async (
+  accessToken: string,
+): Promise<KakaoSignInResponse | undefined> => {
   try {
-    const response = await client.get('/user/kakao/signin', {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+    const response: AxiosResponse<KakaoSignInResponse> =
+      await generalAxios.post(
+        '/user/kakao/signin',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      );
     return response.data;
   } catch (e) {
     console.error(e);
