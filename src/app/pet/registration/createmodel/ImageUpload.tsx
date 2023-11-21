fe71c662e786630daee76ce3de619d236d6bd52a
parkhyeonji <iamphj3@naver.com>
2023-11-21 19:34:13 +0900

    :recycle: 데이터셋 생성 및 이미지 업로드 로직 훅으로 분리(#55)

--- src/app/pet/registration/createmodel/ImageUpload.tsx
+++ src/app/pet/registration/createmodel/ImageUpload.tsx
@@ -1,18 +1,16 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect, useRef, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { styled } from 'styled-components';
 
 import { BottomButton } from '@/components/button';
 import { useMultipleImageUpload } from '@/hooks/image';
 import { useModal } from '@/hooks/modal';
-import { useToast } from '@/hooks/toast';
 import { uploadImagesState } from '@/recoil/createmodel/atom';
 
-import useCreateDataset from '../hooks/useCreateDataset';
-import useUploadDatasetImages from '../hooks/useUploadDatasetImages';
+import useDatasetUpload from '../hooks/useDatasetUpload';
 import ImageConfirm from './ImageConfirm';
 import ImageGuide from './ImageGuide';
 
@@ -22,16 +20,15 @@ const ImageUpload = () => {
   const [validatedImages, setValidatedImages] =
     useRecoilState<File[]>(uploadImagesState);
   const imageInputRef = useRef<HTMLInputElement>(null);
-  const { openModal, closeModal } = useModal();
-  const { showToast } = useToast();
 
-  const router = useRouter();
   const params = useSearchParams();
   const petId = Number(params.get('petId'));
 
-  const { createDataset, datasetId } = useCreateDataset();
-  const { uploadDatasetImages, isLoading } = useUploadDatasetImages();
-  const [isMount, setIsMount] = useState(false);
+  const { openModal, closeModal } = useModal();
+  const { handleDatasetUpload, isLoading } = useDatasetUpload({
+    petId,
+    files: validatedImages,
+  });
 
   const handleUploadImage = () => {
     imageInputRef.current?.click();
@@ -64,28 +61,6 @@ const ImageUpload = () => {
     }
     setValidatedImages(uploadImages);
   };
-
-  const handleDatasetUpload = async () => {
-    try {
-      await uploadDatasetImages({ datasetId, files: validatedImages });
-      // TODO : 완료됐을때 라우팅 처리 필요 (마이페이지 / 주문하기)
-      router.push('/mypage');
-    } catch (error) {
-      showToast('dataset_upload_error');
-      console.error('이미지 업로드 실패', error);
-    }
-  };
-
-  useEffect(() => {
-    setIsMount(true);
-  }, []);
-
-  useEffect(() => {
-    if (isMount) {
-      console.log('마운트');
-      createDataset(petId);
-    }
-  }, [isMount]);
 
   useEffect(() => {
     if (uploadImages.length > 0) {
