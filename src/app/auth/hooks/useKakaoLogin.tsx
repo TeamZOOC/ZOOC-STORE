abc6ed026c68bc27583717d34f016c4d725cb8f3
parkhyeonji <iamphj3@naver.com>
2023-11-22 15:55:55 +0900

    :test_tube: 프로덕션 테스트

--- src/app/auth/hooks/useKakaoLogin.tsx
+++ src/app/auth/hooks/useKakaoLogin.tsx
@@ -1,8 +1,8 @@
-// import { setCookie } from 'cookies-next';
+import { setCookie } from 'cookies-next';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
-// import { kakaoSignIn } from '@/apis/auth';
+import { kakaoSignIn } from '@/apis/auth';
 
 export async function useKakaoLogin() {
   const router = useRouter();
@@ -12,19 +12,19 @@ export async function useKakaoLogin() {
     console.log('session', session);
     if (session.provider === 'kakao') {
       console.log('카카오 로그인', session.accessToken);
-      // const response = await kakaoSignIn(session.accessToken);
-      // if (response) {
-      //   setCookie('accessToken', response.data.accessToken);
-      //   if (response.data.isExistedUser) {
-      //     router.push('/');
-      //   } else {
-      //     router.push('/auth/signup');
-      //   }
-      // }
+      const response = await kakaoSignIn(session.accessToken);
+      if (response) {
+        setCookie('accessToken', response.data.accessToken);
+        if (response.data.isExistedUser) {
+          router.push('/');
+        } else {
+          router.push('/auth/signup');
+        }
+      }
     } else if (session.provider === 'apple') {
       // session.provider === "apple" 일 때
       console.log('애플 로그인', session.accessToken);
-      router.push('/auth/signup');
+      // router.push('/auth/signup');
     }
   }
 }
