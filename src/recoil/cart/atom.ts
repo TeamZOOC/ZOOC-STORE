7f2467805bce4e74dec400037800edf9700922e6
Dev-jwJeong <rgn95652@gmail.com>
2023-11-23 17:16:09 +0900

    :sparkles: 장바구니 recoil-persist 추가(#72)

--- src/recoil/cart/atom.ts
+++ src/recoil/cart/atom.ts
@@ -1,15 +1,21 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 import { OptionInfo } from '../option/atom';
 
-interface cartInfo {
+export interface cartInfo {
   id: number;
   image: string;
   name: string;
   price: number;
   sale: number | null;
-  optionList: Array<OptionInfo[]>;
+  optionList: OptionInfo[];
 }
+const { persistAtom } = recoilPersist({
+  key: 'cart',
+  storage: localStorage,
+});
 export const cartState = atom<cartInfo[]>({
   key: 'cartState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
