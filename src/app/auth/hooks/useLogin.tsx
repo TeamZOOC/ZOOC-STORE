9ef2a3d4d0b3074f7965efae4e9d0157c26a05b6
parkhyeonji <iamphj3@naver.com>
2023-11-23 20:32:32 +0900

    :bug: 테스트

--- src/app/auth/hooks/useLogin.tsx
+++ src/app/auth/hooks/useLogin.tsx
@@ -8,30 +8,26 @@ export const useLogin = async () => {
   const router = useRouter();
   const { data: session } = useSession();
 
-  try {
-    let response;
+  if (session?.accessToken) {
+    if (session.provider === 'kakao') {
+      const response = await kakaoSignIn(session.accessToken);
 
-    switch (session?.provider) {
-      case 'kakao':
-        response = await kakaoSignIn(session.accessToken);
-        break;
-
-      case 'apple':
-        if (!session.email || !session.sub) return;
-        response = await appleSignIn(session.email, session.sub);
-        break;
-
-      default:
-        return;
+      if (response) {
+        setCookie('accessToken', response.data.accessToken);
+        const routePath = response.data.isExistedUser ? '/' : '/auth/signup';
+        router.push(routePath);
+      }
     }
-    console.log(session, response);
 
-    if (response) {
-      setCookie('accessToken', response.data.accessToken);
-      const routePath = response.data.isExistedUser ? '/' : '/auth/signup';
-      router.push(routePath);
+    if (session.provider === 'apple') {
+      if (!session.email || !session.sub) return;
+      const response = await appleSignIn(session.email, session.sub);
+
+      if (response) {
+        setCookie('accessToken', response.data.accessToken);
+        const routePath = response.data.isExistedUser ? '/' : '/auth/signup';
+        router.push(routePath);
+      }
     }
-  } catch (error) {
-    console.error('로그인 실패', error);
   }
 };
