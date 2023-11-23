17eaa3f5e43d0e29fa379a2148ecc1e2c4403c34
parkhyeonji <iamphj3@naver.com>
2023-11-23 12:54:57 +0900

    :sparkles: FITAPAT Url 추가(#76)

---
+++ src/constants/fitapatUrls.ts
@@ -0,0 +1,10 @@
+export const FITAPAT_TERMS_OF_SERVICE =
+  'https://melt-split.notion.site/8c417ba2156d4720b77f480d80ace49d';
+export const FITAPAT_PRIVACY_POLICY =
+  'https://melt-split.notion.site/65a11bf765b748678c8e6345cbfc6140?pvs=4';
+export const FITAPAT_INTRODUCE = 'https://www.instagram.com/fitapat.official/';
+export const FITAPAT_INSTAGRAM = 'https://www.instagram.com/fitapat.official/';
+export const FITAPAT_APP_STORE_LINK =
+  'https://apps.apple.com/kr/app/zooc/id1669547675';
+export const FITAPAT_GOOGLE_PLAY_LINK =
+  'https://play.google.com/store/apps/details?id=org.sopt.zooczoocbbangbbang';
