6945ef731fb28a09669f061bab6be100b5682b85
Dev-jwJeong <rgn95652@gmail.com>
2023-11-23 19:45:56 +0900

    :sparkles: 카테고리 상품 보기 api loading 상태 추가(#85)

--- src/app/product/hooks/useGetProduct.tsx
+++ src/app/product/hooks/useGetProduct.tsx
@@ -3,13 +3,14 @@ import { ProductInfoResponse } from '@/types/product';
 import { useQuery } from '@tanstack/react-query';
 
 const useGetProduct = (categoryName: string) => {
-  const { data: productList } = useQuery<ProductInfoResponse[]>(
+  const { data: productList, isLoading } = useQuery<ProductInfoResponse[]>(
     ['product', categoryName],
     () => getProduct(categoryName),
   );
 
   return {
     productList,
+    isLoading,
   };
 };
 
