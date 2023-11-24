a0ff492ba241eb505611fc21f6d0e4702f660afb
Dev-jwJeong <rgn95652@gmail.com>
2023-11-24 16:17:18 +0900

    :sparkles: 약관 동의 선택 조건 로직 추가(#94)

--- src/constants/agreement.ts
+++ src/constants/agreement.ts
@@ -1,26 +1,30 @@
 export const AGREEMENT = [
   {
     id: 1,
-    agreementTitle: 'ZOOC 이용약관 (필수)',
+    agreementTitle: 'fitapat 이용약관 (필수)',
     agreementLink:
       'https://melt-split.notion.site/8c417ba2156d4720b77f480d80ace49d',
+    show: true,
   },
   {
     id: 2,
     agreementTitle: '개인정보 이용약관 동의 (필수)',
     agreementLink:
       'https://melt-split.notion.site/65a11bf765b748678c8e6345cbfc6140?pvs=4',
+    show: true,
   },
   {
     id: 3,
-    agreementTitle: '마케팅 정보 수신 동의 (필수)',
-    agreementLink:
-      'https://melt-split.notion.site/3-f551ffc7774e4560a3a3cc17dd3c39ef',
-  },
-  {
-    id: 4,
     agreementTitle: '만 14세 이상 확인 (필수)',
     agreementLink:
       'https://melt-split.notion.site/8c417ba2156d4720b77f480d80ace49d',
+    show: false,
+  },
+  {
+    id: 4,
+    agreementTitle: '마케팅 정보 수신 동의 (선택)',
+    agreementLink:
+      'https://melt-split.notion.site/020167668a2b44e08dc15d95b0004643',
+    show: true,
   },
 ];
