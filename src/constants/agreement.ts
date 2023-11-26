65033428695875c0a5e5973104edc27c627f7132
Dev-jwJeong <rgn95652@gmail.com>
2023-11-26 21:27:10 +0900

    :sparkles: 약관동의 만 14세 이상 보기 링크 추가(#148)

--- src/constants/agreement.ts
+++ src/constants/agreement.ts
@@ -4,27 +4,23 @@ export const AGREEMENT = [
     agreementTitle: 'fitapat 이용약관 (필수)',
     agreementLink:
       'https://melt-split.notion.site/8c417ba2156d4720b77f480d80ace49d',
-    show: true,
   },
   {
     id: 2,
     agreementTitle: '개인정보 이용약관 동의 (필수)',
     agreementLink:
       'https://melt-split.notion.site/65a11bf765b748678c8e6345cbfc6140?pvs=4',
-    show: true,
   },
   {
     id: 3,
     agreementTitle: '만 14세 이상 확인 (필수)',
     agreementLink:
-      'https://melt-split.notion.site/8c417ba2156d4720b77f480d80ace49d',
-    show: false,
+      'https://melt-split.notion.site/melt-split/Fitapat-db143aea709a452ba5a91f7dc31aad0f',
   },
   {
     id: 4,
     agreementTitle: '마케팅 정보 수신 동의 (선택)',
     agreementLink:
       'https://melt-split.notion.site/020167668a2b44e08dc15d95b0004643',
-    show: true,
   },
 ];
