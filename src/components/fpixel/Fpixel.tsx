10b6ef94cc7bfa829c11105129e484ce67469f73
Dev-jwJeong <rgn95652@gmail.com>
2023-11-26 23:50:54 +0900

    :sparkles: 페이지 변경 감지 기능 추가(#152)

---
+++ src/components/fpixel/Fpixel.tsx
@@ -0,0 +1,34 @@
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import Script from 'next/script';
+import * as fbq from '@/utils/fpixel';
+
+const Fpixel = () => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    fbq.pageview();
+  }, [pathname, searchParams]);
+
+  return (
+    <Script
+      id="fb-pixel"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `!function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1037687987375235');
+        fbq('track', 'PageView');`,
+      }}
+    />
+  );
+};
+
+export default Fpixel;
