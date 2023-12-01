c264ba27ab054eef1bc80ca2c6506345c706de01
parkhyeonji <iamphj3@naver.com>
2023-12-01 15:28:53 +0900

    :sparkles: 채널톡 추가

--- src/app/ChangeRoute.tsx
+++ src/app/ChangeRoute.tsx
@@ -12,12 +12,16 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { returnPathState } from '@/recoil/order/atom';
 import { userState } from '@/recoil/user/atom';
 
+import useChannelTalk from './useChannelTalk';
+
 const ChangeRoute = () => {
   const [returnPath, setReturnPath] = useRecoilState(returnPathState);
   const userStatus = useRecoilValue(userState);
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
+  useChannelTalk();
 
   useEffect(() => {
     if (!returnPath) return;
@@ -39,7 +43,59 @@ const ChangeRoute = () => {
     };
   }, [pathname, searchParams]);
 
-  return <></>;
+  return (
+    <>
+      {/* <Script
+        id="channeltalk"
+        onError={(e) => {
+          console.error('Script failed to load', e);
+        }}
+        dangerouslySetInnerHTML={{
+          __html: `     
+   (function() {
+    var w = window;
+    if (w.ChannelIO) {
+      return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+    }
+    var ch = function() {
+      ch.c(arguments);
+    };
+    ch.q = [];
+    ch.c = function(args) {
+      ch.q.push(args);
+    };
+    w.ChannelIO = ch;
+    function l() {
+      if (w.ChannelIOInitialized) {
+        return;
+      }
+      w.ChannelIOInitialized = true;
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+      s.charset = 'UTF-8';
+      var x = document.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    }
+    if (document.readyState === 'complete') {
+      l();
+    } else if (window.attachEvent) {
+      window.attachEvent('onload', l);
+    } else {
+      window.addEventListener('DOMContentLoaded', l, false);
+      window.addEventListener('load', l, false);
+    }
+  })();
+  ChannelIO('boot', {
+    "pluginKey": "282c6661-b5a5-480f-845a-abc77adaf3d4", //please fill with your plugin key
+    "memberId": "guswl733598", //fill with user id
+  });
+  `,
+        }}
+      /> */}
+    </>
+  );
 };
 
 export default ChangeRoute;
