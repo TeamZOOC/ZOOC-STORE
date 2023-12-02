5c6284034e1067b0233aa098868bfa5259cd767e
Dev-jwJeong <rgn95652@gmail.com>
2023-12-02 15:20:31 +0900

    :sparkles: Progress bar 적용(#209)

--- src/app/article/series/ArticleSeriesHeaderContainer.tsx
+++ src/app/article/series/ArticleSeriesHeaderContainer.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { Header } from '@/components/header';
+import useProgress from '@/hooks/progress/useProgress';
 import { useRouter } from 'next/navigation';
 
 const ArticleSeriesHeaderContainer = () => {
   const router = useRouter();
+  useProgress();
   return (
     <Header
       headerTitle="나에게 fit 한 설렘, 핏어팻 시리즈"
