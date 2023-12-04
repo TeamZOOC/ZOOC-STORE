3fe17a0884d38dd056e82d7c2efebb1673677605
Dev-jwJeong <rgn95652@gmail.com>
2023-12-05 00:06:47 +0900

    :clown_face: 리뷰 목록 상수(#213)

---
+++ src/constants/review.ts
@@ -0,0 +1,27 @@
+import {
+  ImageReviewDanbi,
+  ImageReviewDanbi1,
+  ImageReviewDanbi2,
+  ImageReviewDanbi3,
+  ImageReviewTori,
+} from '../../public/images';
+
+export const REVIEW = [
+  {
+    id: 1,
+    imageSrc: [ImageReviewTori],
+    reviewTitle: '토리 / ',
+    reviewDesc: '크리스마스 커스텀 휴대폰 케이스',
+  },
+  {
+    id: 2,
+    imageSrc: [
+      ImageReviewDanbi,
+      ImageReviewDanbi1,
+      ImageReviewDanbi2,
+      ImageReviewDanbi3,
+    ],
+    reviewTitle: '단비 / ',
+    reviewDesc: '크리스마스 커스텀 휴대폰 케이스',
+  },
+];
